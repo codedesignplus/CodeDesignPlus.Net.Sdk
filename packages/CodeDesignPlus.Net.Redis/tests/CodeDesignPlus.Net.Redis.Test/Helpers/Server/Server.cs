7981c32b3f7bc4a4e69a802b4615c3995d152a23
CodeDesignPlus <codedesignplus@outlook.com>
2023-10-13 22:03:16 -0500

    test(net.redis): :construction: add server redis with tls and doctus fluent docker

---
+++ packages/CodeDesignPlus.Net.Redis/tests/CodeDesignPlus.Net.Redis.Test/Helpers/Server/Server.cs
@@ -0,0 +1,31 @@
+﻿using Ductus.FluentDocker.Model.Common;
+using Ductus.FluentDocker.Model.Compose;
+using Ductus.FluentDocker.Services;
+using Ductus.FluentDocker.Services.Impl;
+
+namespace CodeDesignPlus.Net.Redis.Test.Helpers.Server;
+
+public class Server : DockerCompose
+{
+    public Server()
+    {
+
+    }
+
+    protected override ICompositeService Build()
+    {
+        var file = Path.Combine(Directory.GetCurrentDirectory(), "Helpers", "Server", (TemplateString)"docker-compose.yml");
+
+        var compose = new DockerComposeCompositeService(
+            base.DockerHost,
+            new DockerComposeConfig
+            {
+                ComposeFilePath = new List<string> { file },
+                ForceRecreate = true,
+                RemoveOrphans = true,
+                StopOnDispose = true
+            });
+
+        return compose;
+    }
+}
--- packages/CodeDesignPlus.Net.Redis/tests/CodeDesignPlus.Net.Redis.Test/RedisServiceTest.cs
+++ packages/CodeDesignPlus.Net.Redis/tests/CodeDesignPlus.Net.Redis.Test/RedisServiceTest.cs
@@ -1,11 +1,24 @@
 ﻿using CodeDesignPlus.Net.Redis.Options;
 using CodeDesignPlus.Net.Redis.Services;
+using CodeDesignPlus.Net.Redis.Test.Helpers.Server;
+using Ductus.FluentDocker.Model.Common;
 using Microsoft.Extensions.Logging;
 using Moq;
 
 namespace CodeDesignPlus.Net.Redis.Test;
 
-public class RedisServiceTest
+public class RedisServiceTest : IClassFixture<Server>
 {
+    private readonly Server fixture;
 
+    public RedisServiceTest(Server fixture)
+    {
+        this.fixture = fixture;
+    }
+
+    [Fact]
+    public void Test()
+    {
+        var file = Path.Combine(Directory.GetCurrentDirectory(), "Helpers", "Server", (TemplateString)"docker-compose.yml");
+    }
 }
