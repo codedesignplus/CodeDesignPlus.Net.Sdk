fe54da6d1166004c476fa0de6cf1f33e2013ec9b
CodeDesignPlus <codedesignplus@outlook.com>
2023-10-27 11:01:23 -0500

    feat(net.xunit): :sparkles: add event store container

---
+++ packages/CodeDesignPlus.Net.xUnit/src/CodeDesignPlus.Net.xUnit/Helpers/EventStoreContainer/EventStoreContainer.cs
@@ -0,0 +1,26 @@
+﻿using Ductus.FluentDocker.Model.Common;
+using Ductus.FluentDocker.Model.Compose;
+using Ductus.FluentDocker.Services;
+using Ductus.FluentDocker.Services.Impl;
+
+namespace CodeDesignPlus.Net.xUnit.Helpers.EventStoreContainer;
+
+public class EventStoreContainer : DockerCompose
+{
+    protected override ICompositeService Build()
+    {
+        var file = Path.Combine(Directory.GetCurrentDirectory(), "Helpers", "EventStoreContainer", (TemplateString)"docker-compose.yml");
+
+        var compose = new DockerComposeCompositeService(
+            base.DockerHost,
+            new DockerComposeConfig
+            {
+                ComposeFilePath = new List<string> { file },
+                ForceRecreate = true,
+                RemoveOrphans = true,
+                StopOnDispose = true
+            });
+
+        return compose;
+    }
+}
