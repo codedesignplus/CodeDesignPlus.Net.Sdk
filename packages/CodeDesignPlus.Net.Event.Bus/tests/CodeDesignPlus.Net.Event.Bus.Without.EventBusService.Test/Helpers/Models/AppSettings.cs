4186b18409a902524b6b90be559132347e8e3375
CodeDesignPlus <codedesignplus@outlook.com>
2023-11-01 08:25:42 -0500

    refactor(net.event.bus): :recycle: The event bus has been removed because it was replaced by PubSub.

--- packages/CodeDesignPlus.Net.Event.Bus/tests/CodeDesignPlus.Net.Event.Bus.Without.EventBusService.Test/Helpers/Models/AppSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿using CodeDesignPlus.Net.Event.Bus.Options;
-
-namespace CodeDesignPlus.Event.Bus.Without.EventBusService.Test.Helpers.Models;
-
-public class AppSettings
-{
-    public EventBusOptions? EventBus { get; set; }
-}
