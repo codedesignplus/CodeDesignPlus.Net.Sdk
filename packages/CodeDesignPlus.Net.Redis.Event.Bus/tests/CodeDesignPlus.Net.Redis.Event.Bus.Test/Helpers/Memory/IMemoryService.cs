bfb4d3ba27fb6d1f57d76591fe379f20c6de524e
CodeDesignPlus <codedesignplus@outlook.com>
2023-11-01 08:37:31 -0500

    refactor(net.redis.event.bus): :construction: the redis event bus has been removed becouse it's was replaces by redis pub sub

--- packages/CodeDesignPlus.Net.Redis.Event.Bus/tests/CodeDesignPlus.Net.Redis.Event.Bus.Test/Helpers/Memory/IMemoryService.cs
+++
@@ -1,9 +0,0 @@
-﻿using CodeDesignPlus.Net.Redis.Event.Bus.Test.Helpers.Events;
-
-namespace CodeDesignPlus.Net.Redis.Event.Bus.Test.Helpers.Memory
-{
-    public interface IMemoryService
-    {
-        List<UserCreatedEvent> UserEventTrace { get; }
-    }
-}
