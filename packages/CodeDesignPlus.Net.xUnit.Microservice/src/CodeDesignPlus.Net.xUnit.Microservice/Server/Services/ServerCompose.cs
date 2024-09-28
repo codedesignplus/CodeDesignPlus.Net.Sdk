cf55e468e89f825410d1acb2a8302489680c6128
CodeDesignPlus <codedesignplus@outlook.com>
2024-09-28 13:40:11 -0500

    fix: :adhesive_bandage: Added support for both local and NuGet paths for `docker-compose.yml` in Build method. Checks `content` folder first, then falls back to local path for debugging.

--- packages/CodeDesignPlus.Net.xUnit.Microservice/src/CodeDesignPlus.Net.xUnit.Microservice/Server/Services/ServerCompose.cs
+++ packages/CodeDesignPlus.Net.xUnit.Microservice/src/CodeDesignPlus.Net.xUnit.Microservice/Server/Services/ServerCompose.cs
@@ -34,7 +34,10 @@ public class ServerCompose : DockerCompose
     /// <returns>A new instance of the <see cref="ServerCompose"/> class.</returns>
     protected override ICompositeService Build()
     {
-        var file = Path.Combine(Directory.GetCurrentDirectory(), "Server", "Services", "docker-compose.yml");
+        var file = Path.Combine(Directory.GetCurrentDirectory(), "content", "docker-compose.yml");
+
+        if (!File.Exists(file))
+            file = Path.Combine(Directory.GetCurrentDirectory(), "Server", "Services", "docker-compose.yml");
 
         var dockerCompose = new DockerComposeConfig
         {
