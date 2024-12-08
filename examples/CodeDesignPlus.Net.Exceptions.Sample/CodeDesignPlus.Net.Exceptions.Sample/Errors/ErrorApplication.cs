0e5fc8aa249fa09632baf362b75a0b5d461f1ce8
CodeDesignPlus <codedesignplus@outlook.com>
2024-12-08 18:33:53 -0500

    chore(net.exceptions): add sample exception

---
+++ examples/CodeDesignPlus.Net.Exceptions.Sample/CodeDesignPlus.Net.Exceptions.Sample/Errors/ErrorApplication.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace CodeDesignPlus.Net.Exceptions.Sample.Errors;
+
+public static class ErrorApplication
+{
+    public const string UserIsRequired = "201: User is required";
+}
