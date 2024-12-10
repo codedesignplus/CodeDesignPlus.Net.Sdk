b45733854fd22a343e130fd8e702e71484a3de15
CodeDesignPlus <codedesignplus@outlook.com>
2024-12-10 07:27:31 -0500

    chore(microservice.commons): :construction: add samples

---
+++ examples/CodeDesignPlus.Net.Microservice.Commons.Sample/src/CodeDesignPlus.Net.Microservice.Rest.Sample/Program.cs
@@ -0,0 +1,31 @@
+using CodeDesignPlus.Net.Microservice.Commons.EntryPoints.Rest.Middlewares;
+using CodeDesignPlus.Net.Microservice.Commons.EntryPoints.Rest.Swagger;
+using CodeDesignPlus.Net.Microservice.Commons.FluentValidation;
+using CodeDesignPlus.Net.Microservice.Commons.MediatR;
+using CodeDesignPlus.Net.Microservice.Sample;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddCoreSwagger<Program>(builder.Configuration);
+
+builder.Services.AddFluentValidation();
+builder.Services.AddMediatR<Startup>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseCoreSwagger();
+}
+
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
