8742d5328b1db95a07b41e423287c92f2c39542e
CodeDesignPlus <codedesignplus@outlook.com>
2025-01-06 06:57:25 -0500

    feat: add ConfirmPassword property to CreateUserCommand and improve error handling in serializer registration

--- samples/CodeDesignPlus.Net.Generator.Sample/src/CodeDesignPlus.Net.Generator.Sample/Program.cs
+++ samples/CodeDesignPlus.Net.Generator.Sample/src/CodeDesignPlus.Net.Generator.Sample/Program.cs
@@ -1,16 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//using CodeDesignPlus.Microservice.Api.Dtos;
+using CodeDesignPlus.Microservice.Api.Dtos;
 
-// CodeDesignPlus.Microservices
+var dto = new CreateUserDto()
+{
+    Name = "John",
+    Email = "john.doe@codedesignplus.com",
+    LastName = "Doe",
+    Birthdate = new DateTime(1990, 10, 10)
+};
 
-//var dto = new CreateUserDto()
-//{
-//    Name = "John",
-//    Email = "john.doe@codedesignplus.com",
-//    LastName = "Doe",
-//    Birthdate = new DateTime(1990, 10, 10)
-//};
-
-//Console.WriteLine(dto.Name);
+Console.WriteLine(dto.Name);
 
 Console.ReadLine();
