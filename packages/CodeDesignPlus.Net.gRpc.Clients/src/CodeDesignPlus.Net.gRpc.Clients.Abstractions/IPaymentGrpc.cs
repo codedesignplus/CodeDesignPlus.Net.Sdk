d61589cadcee5b38df23869708875ee9ef29e93f
Wilzon <wliscano@codedesignplus.com>
2025-06-27 21:09:21 -0500

    feat: rename PayAsync to InitiatePaymentAsync and add UpdateStatusAsync method for payment status updates

--- packages/CodeDesignPlus.Net.gRpc.Clients/src/CodeDesignPlus.Net.gRpc.Clients.Abstractions/IPaymentGrpc.cs
+++ packages/CodeDesignPlus.Net.gRpc.Clients/src/CodeDesignPlus.Net.gRpc.Clients.Abstractions/IPaymentGrpc.cs
@@ -13,5 +13,15 @@ public interface IPaymentGrpc
     /// <param name="request">The request containing payment information.</param>
     /// <param name="cancellationToken">Cancellation token to observe while waiting for the task to complete.</param>
     /// <returns>Returns a task representing the asynchronous operation.</returns>
-    Task PayAsync(InitiatePaymentRequest request, CancellationToken cancellationToken);
+    Task InitiatePaymentAsync(InitiatePaymentRequest request, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Updates the status of a payment.
+    /// This method is used to update the status of a payment after it has been processed.
+    /// It is typically called by the payment gateway or service to notify the system of the payment status change.
+    /// </summary>
+    /// <param name="id">The unique identifier of the payment to update.</param>
+    /// <param name="cancellationToken">Cancellation token to observe while waiting for the task to complete.</param>
+    /// <returns>Returns a task representing the asynchronous operation.</returns>
+    Task<UpdateStatusResponse> UpdateStatusAsync(Guid id, CancellationToken cancellationToken);
 }
