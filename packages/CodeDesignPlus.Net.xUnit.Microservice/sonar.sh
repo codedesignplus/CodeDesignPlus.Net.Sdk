38ee17c0de10566bf2378a7c0817abfe9b023738
Wilzon Camilo Liscano Galindo <wliscano@codedesignplus.com>
2025-07-06 11:47:51 -0500

    Add SonarQube token to sonar scanner commands for various projects
    - Updated sonar.sh and project.json files across multiple packages to include the SonarQube token for authentication during the scanning process.
    - Ensured coverage exclusions and report paths are correctly set for each project.
    - This change enhances security by requiring a token for SonarQube operations, improving the integrity of the CI/CD pipeline.

--- packages/CodeDesignPlus.Net.xUnit.Microservice/sonar.sh
+++ packages/CodeDesignPlus.Net.xUnit.Microservice/sonar.sh
@@ -13,4 +13,4 @@ server=https://sonarcloud.io
 dotnet test $csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 dotnet sonarscanner begin /o:$org /k:$key /d:sonar.host.url=$server /d:sonar.coverage.exclusions="**Tests*.cs" /d:sonar.cs.opencover.reportsPaths=$report
 dotnet build
-dotnet sonarscanner end
+dotnet sonarscanner end /d:sonar.token=sqa_44c22c45e0ee901f36b1d6b446b17ad7be3f589e
