e25d0c73a4f90d76bd3b92c3f7bfedbdd5a2d954
Wilzon Camilo Liscano Galindo <wliscano@codedesignplus.com>
2026-02-26 21:53:17 -0500

    feat: enhance ICurrencyGrpc interface with detailed XML documentation

--- packages/CodeDesignPlus.Net.gRpc.Clients/src/CodeDesignPlus.Net.gRpc.Clients.Abstractions/ICurrencyGrpc.cs
+++ packages/CodeDesignPlus.Net.gRpc.Clients/src/CodeDesignPlus.Net.gRpc.Clients.Abstractions/ICurrencyGrpc.cs
@@ -1,8 +1,19 @@
-using System;
+using CodeDesignPlus.Net.gRpc.Clients.Services.Currencies;
 
 namespace CodeDesignPlus.Net.gRpc.Clients.Abstractions;
 
+/// <summary>
+/// Defines the contract for interacting with currency-related gRPC services.
+/// </summary>
 public interface ICurrencyGrpc
 {
 
+    /// <summary>
+    /// Retrieves currency information.
+    /// </summary>
+    /// <param name="request">The request containing currency information.</param>
+    /// <param name="cancellationToken">Cancellation token to observe while waiting for the task to complete.</param>
+    /// <returns>Returns a task representing the asynchronous operation.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when the authorization header is missing.</exception>
+    Task<GetCurrencyResponse> GetCurrencyAsync(GetCurrencyRequest request, CancellationToken cancellationToken);
 }
